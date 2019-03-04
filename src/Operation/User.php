749fcac70f0712930e889f436a8445a0525173a9
Steve <sj.worley88@gmail.com>
2019-03-05 00:01:09 +1000

    Tests (#5)
    * Add test coverage for the mutations and queries.
    
    * Add circle config.

---
+++ src/Operation/User.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Lagoon\Operation;
+
+use Lagoon\Operation\LagoonOperationBase;
+use Lagoon\Query\Users\UsersBySshKey;
+
+/**
+ * Notification graphql operations.
+ */
+class User extends LagoonOperationBase {
+
+  /**
+   * Naming constants to use throughout the class.
+   */
+  const SSH_KEY = 'find_ssh_key';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function bind() {
+    $this->addQuery(self::SSH_KEY, UsersBySshKey::class);
+  }
+
+  /**
+   * Execute the add slack mutation.
+   *
+   * @param array $variables
+   *   A list of variables to add.
+   *
+   * @return Lagoon\LagoonResult
+   *   The lagoon result object.
+   */
+  public function withKey($key = '') {
+    return $this->query(SELF::UsersBySshKey, ['sshKey' => $key]);
+  }
+}
