0be112fc2c81aa3dca05b4346ade04d9d0efe67c
Steve Worley <sj.worley88@gmail.com>
2019-03-07 14:11:18 +1000

    Update the query to update projects.
    - Changes the query to Int! for required fields.

--- src/Mutation/Project/Update.php
+++ src/Mutation/Project/Update.php
@@ -32,11 +32,11 @@ class Update extends LagoonQueryBase {
 mutation {
   addProject(
     input: {
-      \$id: Int,
+      \$id: Int!,
       patch: {
         \$name: String!,
-        \$customer: String!,
-        \$openshift: String!,
+        \$customer: Int!,
+        \$openshift: Int!,
         \$gitUrl: String!,
         \$productionEnvironment: String!,
         \$branches: String!
