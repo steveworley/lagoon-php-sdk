049efc5581f610f7e887480040cb8e58873ea66d
Steve Worley <sj.worley88@gmail.com>
2019-06-13 17:55:29 +1000

    Adds update query support.

---
+++ src/Mutation/Environment/Update.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Lagoon\Mutation\Environment;
+
+use Lagoon\LagoonQueryBase;
+
+/**
+ * Update a project.
+ */
+class Update extends LagoonQueryBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function expectedKeys(array $variables = []) {
+    return [
+      'id',
+      'patch',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function query() {
+    return <<<QUERY
+mutation UpdateEnvironment(
+  \$id: Int!
+  \$patch: UpdateEnvironmentPatchInput!
+) {
+  %s
+}
+QUERY;
+  }
+
+}
