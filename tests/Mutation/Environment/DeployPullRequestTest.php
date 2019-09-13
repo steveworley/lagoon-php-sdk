fa2264c7b530774e3cbd0a98840efc3194b269de
Steve <sj.worley88@gmail.com>
2019-09-13 13:38:57 +1000

    Add deploy pr command. (#20)

---
+++ tests/Mutation/Environment/DeployPullRequestTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Lagoon\Test\Mutation\Environment;
+
+use PHPUnit\Framework\TestCase;
+use Lagoon\Mutation\Environment\DeployPullRequest;
+use Lagoon\LagoonClient;
+
+class DeployPullRequestTest extends TestCase
+{
+
+  protected $client;
+
+  public function callMethod($obj, $name, array $args = [])
+  {
+    $class = new \ReflectionClass($obj);
+    $method = $class->getMethod($name);
+    $method->setAccessible(true);
+    return $method->invokeArgs(new $obj($this->client), $args);
+  }
+
+  public function setup()
+  {
+    $client = $this->getMockBuilder(LagoonClient::class)
+      ->disableOriginalConstructor()
+      ->getMock();
+
+    $this->client = $client;
+  }
+
+  public function testExpectedKeys()
+  {
+    $expected_keys = $this->callMethod(DeployPullRequest::class, 'expectedKeys');
+    $this->assertEquals([
+      'name',
+      'number',
+      'title',
+      'headBranchName',
+      'headBranchRef',
+      'baseBranchName',
+      'baseBranchRef',
+    ], $expected_keys);
+  }
+
+  public function testQuery()
+  {
+    $query = <<<QUERY
+mutation DeployPullRequest(
+  \$name: String!
+  \$number: Int!
+  \$title: String!
+  \$headBranchName: String!
+  \$headBranchRef: String!
+  \$baseBranchName: String!
+) {
+  deployEnvironmentPullrequest(input: {
+    project: {
+      name: \$name
+    }
+    number: \$number
+    title: \$title
+    headBranchName: \$headBranchName
+    headBranchRef: \$headBranchRef
+    baseBranchRef: \$baseBranchSha
+    baseBranchName: \$baseBranchName
+  })
+}
+QUERY;
+
+    $called = $this->callMethod(DeployPullRequest::class, 'query');
+    $this->assertEquals($query, $called);
+  }
+}
